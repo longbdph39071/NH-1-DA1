de6d273ac26da727ba2851dc493a96c862edc67b
dung <dungleph38549fpt.edu.vn>
2024-11-18 19:48:08 +0700

    dung up

--- index.php
+++ index.php
@@ -26,59 +26,8 @@ $arrNeedAuth = [
 
 middleware_auth_check($act, $arrNeedAuth);
 
-match ($act) {
-    // CRUD Trang chủ 
-    '/' => Homeindex(),
 
-    '404' => notFound(),
-
-    // CRUD Danh mục
-    'category' => listCatePro($_GET['id']),
-
-    // CRUD Giới thiệu
-    'about' => About(),
-
-    // CRUD Tin tức
-    'blog' => Blog(),
-
-    // CRUD Liên hệ
-    'contact' => Contact(),
-
-    // CRUD đon hàng
-    'cart-add' => cartAdd($_GET['productID'], $_GET['quantity']),
-    'cart' => cartList(),
-    'cart-inc' => cartInc($_GET['productID']),
-    'cart-dec' => cartDec($_GET['productID']), 
-    'cart-del' => cartDel($_GET['productID']),
-
-    // CRUD đơn hàng
-    'order-checkout' => orderCheckout(),
-    'order-purcharse' => orderPurchase(),
-    'order-success' => orderSuccess(),
-    'order-nhanhang' => ordernhanhang($_GET['id']),
-    'momopayment' => momopayment(),
-
-
-    
-    // CRUD Chi tiết sản phẩm
-    'produc' => shopListAll(),
-    'singer-detail' => singerDetail($_GET['id']),
-    
-    // CRUD Trang admin của người dùng
-    'admin-client' => adminUser(),
-    'history-order' => historyOrder($_GET['id']),
-    'detail-order' => detailOrder($_GET['id']),
-    'detail-order-delete' => deleteOrderUser($_GET['id']),
-
-
-    // CRUD Login
-    'login' => loginuser(),
-    'logout' => logoutuser(),
-    'register' => registeruser(),
-    
-
-};
+  
 
 
 
-require './commons/disconect.php';
